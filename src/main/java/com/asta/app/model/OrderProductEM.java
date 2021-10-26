ce1999c22e32acf1290cc141934eddbc449bc902
marouan <alibellahsen2@gmail.com>
2021-10-26 15:26:25 +0200

    commit

--- src/main/java/com/asta/app/model/OrderProductEM.java
+++ src/main/java/com/asta/app/model/OrderProductEM.java
@@ -2,10 +2,16 @@ package com.asta.app.model;
 
 import java.io.Serializable;
 
+import javax.persistence.Embeddable;
 import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
+
+@Embeddable
+@JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "order")
 public class OrderProductEM implements Serializable {
 
 	private static final long serialVersionUID = 1L;
