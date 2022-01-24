c5f4bddceb74afb3c36b2f0ede8e46f1e898df7c
marouan96 <58396563+marouan96@users.noreply.github.com>
2022-01-24 16:04:49 +0100

    Delete src/main/java/com/asta/app/Repository directory

--- src/main/java/com/asta/app/Repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.asta.app.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.asta.app.model.Cart;
-
-public interface CartRepository extends JpaRepository<Cart, Long>{
-
-}
